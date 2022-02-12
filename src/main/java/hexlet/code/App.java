3b73f3fea86b3ca499dc245d15b32aed7015fa2b
Kirill_Bismark <project1326@mail.ru>
2022-02-12 21:40:15 +1000

    stage 3

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -2,6 +2,7 @@ package hexlet.code;
 
 public class App {
 	public static void main(String[] arg) {
-		System.out.println("Welcome to the Brain Games!");
+		Cli cli = new Cli();
+		cli.constructHello();
 	}
 }
