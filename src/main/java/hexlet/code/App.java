76e82b0308adeb8e8e725ffdd8dca33d075b3f1f
Kirill_Bismark <project1326@mail.ru>
2022-02-23 21:05:02 +1000

    Stage 5. Add Even.java

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,8 +1,40 @@
 package hexlet.code;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] arg) {
-        Cli cli = new Cli();
-        cli.constructHello();
+        Scanner scanner = new Scanner(System.in);
+        Even even = new Even();
+
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("2 - Every");
+        System.out.println("0 - Exit");
+
+        int command = scanner.nextInt();
+        System.out.println("Your choice: " + command);
+
+        switch (command) {
+            case 1:
+                constructHello();
+                break;
+            case 2:
+                even.menu();
+                break;
+            case 0:
+                System.out.println("Exit");
+                break;
+            default:
+                System.out.println("Sorry, command not found");
+                break;
+        }
+    }
+
+    public static void constructHello() {
+        System.out.println("Welcome to the Brain Games!");
+        Scanner input = new Scanner(System.in);
+        System.out.println("May I have your name?");
+        String name = input.nextLine();
+        System.out.println("Hello, " + name + "!");
     }
 }
